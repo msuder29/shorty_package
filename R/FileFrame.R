ab0c12e04b66f129ffe5da5101567fe81d948936
msuder29 <msuder@eastern.edu>
2021-02-20 12:14:22 -0500

    defaulted to working directory

--- R/FileFrame.R
+++ R/FileFrame.R
@@ -8,7 +8,7 @@
 #'
 #'
 #' @export
-file_frame <- function(x) {
+file_frame <- function(x = getwd()) {
   x <- x
   data.frame("File_names" = basename(matrix(unlist(list.files(x,full.names = T)), byrow = T)),
              "File_path" = matrix(unlist(list.files(x,full.names = T)), byrow = T), stringsAsFactors = F)
