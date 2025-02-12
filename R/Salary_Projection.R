454a2335994c3a0e8fcafc354d9363830c70af15
msuder29 <msuder@eastern.edu>
2025-02-11 20:03:00 -0500

    New Function for Salary Projections

---
+++ R/Salary_Projection.R
@@ -0,0 +1,60 @@
+#' Salary Projection Function
+#'
+#' Function will crate projection for salary growth based on a consistent merit increase and bonus struction over input period of time
+#'
+#' @param starting_salary numeric value representing Starting Salary
+#' @param Bonus_pct numeric value that represents annual bonus as a percentage
+#' @param Growth_average numeric value that represents expected yearly merit raise (defualts to 3%)
+#' @param timeframe numeric value that represents number of years for projection
+#'
+#' @export
+
+Salary_Projection <- function(starting_salary, Bonus_pct, Growth_average = 0.03, timeframe) {
+  
+  timeframe <- timeframe
+  
+  starting_salary <- starting_salary
+  
+  Bonus_pct <- Bonus_pct
+  
+  Growth_average <- Growth_average
+  
+  career_trajectory <- data.frame(
+    Year = 0
+    ,Salary = starting_salary
+    ,Bonus = Bonus_pct
+    ,Yearly_Increase = Growth_average
+    ,Total_Yearly_Comp = starting_salary + (starting_salary * Bonus_pct)
+  )
+  
+  counter <- 0
+  
+  while (counter < timeframe) {
+    
+    counter <- counter + 1
+    
+    cs <- max(career_trajectory$Salary)
+    
+    temp <- data.frame(
+      Year = counter
+      ,Salary = cs + (Growth_average * cs)
+      ,Bonus = Bonus_pct
+      ,Yearly_Increase = Growth_average
+      ,Total_Yearly_Comp = cs + (cs * Bonus_pct)
+    )
+    
+    career_trajectory <- rbind(career_trajectory, temp)
+    
+    
+    
+  }
+  
+  
+  assign("Career_Projection",career_trajectory,envir = .GlobalEnv)
+  View(Career_Projection)
+  
+  
+}
+
+
+
